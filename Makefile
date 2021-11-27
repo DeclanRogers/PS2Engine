cd63be2f8bf39056243f99a1b176d8ecc407a197
DeclanRogers <70367466+DeclanRogers@users.noreply.github.com>
2021-11-27 21:29:57 +1100

    Cube + Pad support

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 
 EE_BIN=test.elf
-EE_OBJS=main.o
+EE_OBJS=main.o gs.o mesh.o draw.o math.o pad.o
 
-EE_LIBS=-ldma -lgraph -ldraw -lkernel -ldebug
+EE_LIBS=-ldma -lgraph -ldraw -lkernel -ldebug -lmath3d -lm -lpad
 
 EE_CFLAGS += -Wall --std=c99
 EE_LDFLAGS = -L$(PSDSDK)/ee/common/lib -L$(PS2SDK)/ee/lib
@@ -19,7 +19,7 @@ include $(PS2SDK)/samples/Makefile.pref
 all: $(ISO_TGT)
 
 $(ISO_TGT): $(EE_BIN)
-	mkisofs -l -o $(ISO_TGT) $(EE_BIN) SYSTEM.CNF
+	mkisofs -l -o $(ISO_TGT) $(EE_BIN) SYSTEM.CNF cube.bin
 
 .PHONY: docker-build
 docker-build:
--- main.c
+++ main.c
@@ -12,17 +12,23 @@
 
 #include <inttypes.h>
 
+#include "gs.h"
+#include "mesh.h"
+#include "draw.h"
+#include "log.h"
+#include "pad.h"
+
 #define OFFSET_X 2048
 #define OFFSET_Y 2048
 
-#define myftoi4(x) ((x)<<4)
 
 #define VID_W 640
 #define VID_H 448
 
-static qword_t *buf; 
+#define TGT_FILE "CUBE.BIN"
 
-#define DRAWBUF_LEN (100 * 16)
+#define fatalerror(st, msg, ...) printf("FATAL: " msg "\n", ##__VA_ARGS__); error_forever(st); ((void)0)
+void error_forever(struct draw_state *st);
 
 int print_buffer(qword_t *b, int len)
 {
@@ -35,238 +41,190 @@ int print_buffer(qword_t *b, int len)
   return 0;
 }
 
-int gs_finish()
-{
-  qword_t *q = buf;
-  q = draw_finish(q);
-  dma_channel_send_normal(DMA_CHANNEL_GIF, buf, q-buf, 0, 0);
-  dma_wait_fast();
-  return 0;
-}
-
-
-zbuffer_t *z;
-int gs_init(int width, int height, int psm, int psmz, int vmode, int gmode)
-{
-  framebuffer_t fb;
-  fb.address = graph_vram_allocate(width, height, psm, GRAPH_ALIGN_PAGE);
-  fb.width = width;
-  fb.height = height;
-  fb.psm = psm;
-  fb.mask = 0;
-
-  z->address = graph_vram_allocate(width, height, psmz, GRAPH_ALIGN_PAGE);
-  z->enable = 0;
-  z->method = 0;
-  z->zsm = 0;
-  z->mask = 0;
-
-  graph_set_mode(gmode, vmode, GRAPH_MODE_FIELD, GRAPH_DISABLE); 
-  graph_set_screen(0, 0, width, height);
-  graph_set_bgcolor(0, 0, 0);
-  graph_set_framebuffer_filtered(fb.address, width, psm, 0, 0);
-  graph_enable_output();
-
-  qword_t *q = buf;
-  memset(buf, 0, DRAWBUF_LEN);
-  q = draw_setup_environment(q, 0, &fb, z);
-  q = draw_primitive_xyoffset(q, 0, 2048-(VID_W/2), 2048-(VID_H/2));
-  q = draw_finish(q);
-  dma_channel_send_normal(DMA_CHANNEL_GIF, buf, q-buf, 0, 0);
-  draw_wait_finish();
-
-
-  return 0;
-}
-
-
-static int tri1[] = {
-  0, 0, 0,
-  0, 0, 0,
-  0, 0, 0,
-};
-//static int tri2[] = {
-//  0, 448, 0,
-//  0, 224, 0,
-//  320, 448, 0,
-//};
-//
-//static int tri3[] = {
-//  640, 0, 0,
-//  320, 0, 0,
-//  640, 224, 0,
-//};
-//static int tri4[] = {
-//  640, 448, 0,
-//  320, 448, 0,
-//  640, 224, 0,
-//};
-//
-//static int tri11[] = {
-//  320, 224, 0,
-//  320, 0, 0,
-//  0, 224, 0,
-//};
-//static int tri22[] = {
-//  320, 224, 0,
-//  0, 224, 0,
-//  320, 448, 0,
-//};
-//
-//static int tri33[] = {
-//  320, 224, 0,
-//  320, 0, 0,
-//  640, 224, 0,
-//};
-//static int tri44[] = {
-//  320, 224, 0,
-//  320, 448, 0,
-//  640, 224, 0,
-//};
-
-
-
-#define SHIFT_AS_I64(x, b) (((int64_t)x)<<b)
-
-qword_t *draw(qword_t *q, uint64_t R,uint64_t G,uint64_t B, int tris[])
-{
-  
-  uint64_t red = 0x00 + R;
-  uint64_t blue = 0x00+ G;
-  uint64_t green = 0x00 +B;
-
-  // SET PRIM
-  q->dw[0] = 0x1000000000000001;
-  q->dw[1] = 0x000000000000000e;
-  q++;
-  q->dw[0] = GS_SET_PRIM(GS_PRIM_TRIANGLE, 0, 0, 0, 0, 0, 0, 0, 0);
-  q->dw[1] = GS_REG_PRIM;
-  q++;
-  // 6 regs, x1, EOP
-  q->dw[0] = 0x6000000000008001;
-  // GIFTag header - col, pos, col, pos, col, pos
-  q->dw[1] = 0x0000000000515151;
-  q++;
-
-  int cx = myftoi4(2048 - (VID_W/2));
-  int cy = myftoi4(2048 - (VID_H/2));
-
-  for(int i = 0; i < 3; i++) {
-
-    q->dw[0] = (red&0xff) | (green&0xff)<<32;
-    q->dw[1] = (blue&0xff) | (SHIFT_AS_I64(0x80, 32));
-    q++;
-    
-    // 0xa -> 0xa0
-    // fixed point format - xxxx xxxx xxxx.yyyy
-    int base = i*3;
-    int x = myftoi4(tris[base+0]) + cx;
-    int y = myftoi4(tris[base+1]) + cy;
-    int z = 0;
-    q->dw[0] = x | SHIFT_AS_I64(y, 32);
-    q->dw[1] = z; 
-    // printf("drawing vertex %x %x %x\n", tri[base+0], tri[base+1], tri[base+2]);
-    q++;
-  }
-
-  return q;
-}
-
-uint64_t R= 0;
-uint64_t G= 0;
-uint64_t B= 0;
-
-
-uint64_t R1= 0;
-uint64_t G1= 0;
-uint64_t B1= 0;
-
-
 int main()
 {
-  time_t time;
-  
-  srand(time);
-  
-  buf = malloc(DRAWBUF_LEN);
-  z = malloc(sizeof(zbuffer_t));
+  printf("Hello\n");
+  qword_t *buf = malloc(20000*16);
+  char *file_load_buffer = malloc(310 * 1024);
+  int file_load_buffer_len = 310*1024;
+
+  struct draw_state st = {0};
+  st.width = VID_W,
+  st.height = VID_H,
+  st.vmode = graph_get_region(),
+  st.gmode = GRAPH_MODE_INTERLACED,
+
   // init DMAC
   dma_channel_initialize(DMA_CHANNEL_GIF, 0, 0);
   dma_channel_fast_waits(DMA_CHANNEL_GIF);
 
-  int vmode = graph_get_region();
   // initialize graphics mode 
-  gs_init(VID_W, VID_H, GS_PSM_32, GS_PSMZ_24, vmode, GRAPH_MODE_INTERLACED);
-  int swap = 0;
+  gs_init(&st, GS_PSM_32, GS_PSMZ_24);
+
+  struct model m = {0};
+  struct model m1 = {0};
+  struct model m2 = {0};
+  struct model m3 = {0};
+
+
+
+
+
+  int bytes_read = load_file(TGT_FILE, file_load_buffer, file_load_buffer_len);
+  if (bytes_read <= 0) {
+    fatalerror(&st, "failed to load file %s", TGT_FILE);
+  }
+  if (bytes_read % 16 != 0) {
+    fatalerror(&st, "lengt of model file %s was not 0 %% 16", TGT_FILE);
+  }
+
+  if (!model_load(&m, file_load_buffer, bytes_read)) {
+    fatalerror(&st, "failed to process model");
+  }
+
+    if (!model_load(&m1, file_load_buffer, bytes_read)) {
+    fatalerror(&st, "failed to process model");
+  }
+  struct render_state r = {0};
+
+  r.camera_pos[0] = 0.0f;
+  r.camera_pos[2] = 10.0f;
+  r.camera_pos[3] = 1.0f;
+
+  r.clear_col[0] = 0x2c;
+  r.clear_col[1] = 0x2f;
+  r.clear_col[2] = 0x33;
+
+  r.offset_x = OFFSET_X;
+  r.offset_y = OFFSET_Y;
+
+  struct model_instance cube = {0};
+  cube.m = &m;
+  cube.scale[0] = .8f;
+  cube.scale[1] = .8f;
+  cube.scale[2] = .8f;
+  cube.scale[3] = 1.0f;
+  cube.translate[2] = -10.f;
+
+  pad_init();
+
+int X = 0;
+int T = 0;
+int S = 0;
+int C = 0;
+//int yy = 1;
+//bool zz = false;
   graph_wait_vsync();
-
-
-
-
   while(1) {
-R= rand() % (255 + 1 - 0) + 0; 
-G= rand() % (255 + 1 - 0) + 0;
-B= rand() % (255 + 1 - 0) + 0;
+    pad_frame_start();
+    pad_poll();
+
+
+    X = button_held(0);
+    S = button_held(1);
+    C = button_held(12);
+    T = button_held(11);
+    
+  if(X)
+  {
+    cube.m->r =   0x7C;
+    cube.m->g =   0xB2;
+    cube.m->b =   0xE8;
+  }
+  else if(S)
+  {
+    cube.m->r = 0xFF;
+
+    cube.m->g = 0x69;
+
+    cube.m->b = 0xF8;
+
+  }
+    else if(C)
+  {
+    cube.m->r = 0xFF;
+    cube.m->g = 0x66;
+    cube.m->b = 0x66;
+
+  }
+    else if(T)
+  {
+    cube.m->r = 0x40;
+    cube.m->g = 0xE2;
+    cube.m->b = 0x10;
+
+  }
+  else
+  {
+    cube.m->r = 0xFF;
+    cube.m->g = 0xFF;
+    cube.m->b = 0xFF;
+  }
 
 
 
-//for (size_t i = 0; i < 9; i++)
-//{
- // printf("tri data: %d %d \n",i, tri1[i]);
-//}
 
-
-
+    info("Button X:%d S:%d C:%d T:%d \n", X, S,C,T );
+    update_draw_matrix(&r);
     dma_wait_fast();
     qword_t *q = buf;
-    memset(buf, 0, DRAWBUF_LEN);
-    q = draw_disable_tests(q, 0, z);
-    q = draw_clear(q, 0, 2048.0f - 320, 2048.0f - 244, VID_W, VID_H, 20, 20, 20);
-    q = draw_enable_tests(q, 0, z);
-
-//if(swap %2)
-//{
-int y = 0;
-for (size_t x = 0; x < 3; x++)
-{
- for (size_t y = 0; y < 3; y++)  
- {
-   tri1[0] = 213 * x;
-   tri1[3] = 213 * (x + 1);
-   tri1[6] = tri1[0];
-
-   tri1[1] = 149 * y;
-   tri1[4] = 149 * y;
-   tri1[7] = 149 * (y + 1);
-
-   q = draw(q, R,G,B, tri1);
-     
-
-   printf("%d %d \n", tri1[0], tri1[1]);  
-   printf("%d %d \n", tri1[3], tri1[4]);
-   printf("%d %d \n \n", tri1[6], tri1[7]);
- }
-}
-  //printf("R %d G %d B %d \n", R,G,B);
-swap++;
+    memset(buf, 0, 20000*16);
+    q = draw_disable_tests(q, 0, &st.zb);
+    q = draw_clear(q, 0, 2048.0f - 320, 2048.0f - 244, 
+        VID_W, VID_H, 
+        r.clear_col[0], r.clear_col[1], r.clear_col[2]);
+    q = draw_enable_tests(q, 0, &st.zb);
+    qword_t *model_verts_start = q;
+    memcpy(q, m.buffer, m.buffer_len);
+    info("copied mesh buffer with len=%d", m.buffer_len);
+    q += (m.buffer_len/16);
     q = draw_finish(q);
+  
+      mesh_transform((char*) (model_verts_start+MESH_HEADER_SIZE), &cube, &r);
+    
     dma_channel_send_normal(DMA_CHANNEL_GIF, buf, q-buf, 0, 0);
-    //print_buffer(buf, q-buf); 
+    // print_buffer(buf, q-buf); 
+    info("draw from buffer with length %d", q-buf);
 
     draw_wait_finish();
-    // wait for vsync
     graph_wait_vsync();
-    sleep(1);
+
+    int dx = button_held(DPAD_RIGHT) - button_held(DPAD_LEFT);
+    int dy = button_held(DPAD_DOWN) - button_held(DPAD_UP);
+    int dz = button_held(BUTTON_L1) - button_held(BUTTON_L2); 
+
+      
+    cube.translate[0] += 0.6f * dx;
+    cube.translate[1] += 0.6f * dy;
+    cube.translate[2] += 0.05f * dz;
+    int cdx = button_held(BUTTON_R1) - button_held(BUTTON_R2);
+    r.camera_pos[0] += 0.1f * cdx;
   }
 }
 
-//nt * getRandom()
-//
-// static int r[3];
-//
-//[0] = rand() % (255 + 1 - 0) + 0;
-//[1] = rand() % (255 + 1 - 0) + 0;
-//[2] = rand() % (255 + 1 - 0) + 0;
-// return r;
-//
 
+void error_forever(struct draw_state *st)
+{
+  qword_t *buf = malloc(1200);
+  while(1) {
+    dma_wait_fast();
+    qword_t *q = buf;
+    memset(buf, 0, 1200);
+    q = draw_disable_tests(q, 0, &st->zb);
+    q = draw_clear(q, 0, 2048.0f - 320, 2048.0f - 244, VID_W, VID_H, 0xff, 0, 0);
+    q = draw_finish(q);
+    dma_channel_send_normal(DMA_CHANNEL_GIF, buf, q-buf, 0, 0);
+    draw_wait_finish();
+    graph_wait_vsync();
+    sleep(2);
+  }
+
+
+}
+
+/*
+ *
+ * xxxx xxxx xxxx . yyyy
+ * 0100 0000 0000 . 0000
+ *
+2 */
